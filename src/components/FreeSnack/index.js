2af68f1d4a0397e496514761c6cd2f98808974c9
Andrew Nguyen <andrewnguyen14@gmail.com>
2020-12-03 11:37:11 -0800

    add free snack child component

---
+++ src/components/FreeSnack/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+export default class FreeSnack extends Component {
+
+  render() {
+    let snack = this.props.snack;
+    return (
+      <div className="snack-item-points">
+              TEST
+      </div>
+    );
+  }
+}
