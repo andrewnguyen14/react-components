6a5f4ffa91eec22273f50bf40f968fa0a4f04e77
Andrew Nguyen <andrewnguyen14@gmail.com>
2020-12-04 12:11:20 -0800

    free snack component

--- src/components/FreeSnack/index.js
+++ src/components/FreeSnack/index.js
@@ -6,9 +6,18 @@ export default class FreeSnack extends Component {
   render() {
     let snack = this.props.snack;
     return (
-      <div className="snack-item-points">
-              TEST
+      <div className="snack-item-points"  key={`promo-item-${snack.name}`}>
+        <div className="image-container">
+          <img src={snack.img} alt={snack.name}></img>
+        </div> 
+        <div className="text-container">
+          <div className="text">{snack.name}</div>
+          <div>{snack.points}</div>
+          <button>-</button>
+          <div className="quantity">{this.props.itemQuantity}</div>
+          <button onClick={this.props.addFreeSnack.bind(snack)}>+</button>
+        </div>               
       </div>
     );
   }
-}
+  }
